04ba99b634b8ae9847c70899abdf428ae7b574e9
Nebiy Negash <neba048@gmail.com>
2023-03-26 11:50:44 -0500

    finished app build

--- shopping-list/main.js
+++ shopping-list/main.js
@@ -1,6 +1,6 @@
 //* get our functions from firebase  
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js'
-import { getDatabase, ref, push, onValue} from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js'
+import { getDatabase, ref, push, onValue, remove} from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js'
 
 //* grab the inputfield and button from html
 const inputField = document.querySelector('#input-field');
@@ -18,23 +18,47 @@ const database = getDatabase(app)
 const shoppingListInDB = ref(database, 'shoppingList')
 
 addButton.addEventListener('click',function() {
+
   let inputValue = inputField.value
-  push(shoppingListInDB ,inputValue)
+  if(inputValue === '') {
+    shoppingList === null
+  } else { 
+    push(shoppingListInDB ,inputValue)
   emptyInputField();
+}
+ 
 });
 
 onValue(shoppingListInDB, function(snapshot) {
-  clearListItems();
-  const dbItems = Object.values(snapshot.val())
-  function displayItems(dbItems) {
-    displayListItems(dbItems )
+
+  if (snapshot.exists()) {
+    const dbItems = Object.entries(snapshot.val())
+    clearListItems();
+    for (let i = 0; i < dbItems.length; i++) {
+      let currentItem = dbItems[i]
+      let currentItemID = currentItem[0]
+      let currentItemValue = currentItem[1]
+      displayListItems(currentItem)
+    }
+
+  } else {
+    shoppingList.innerHTML = 'No items here yet!'
   }
-  dbItems.forEach(displayItems)
+
 })
 
-function displayListItems(itemValue) {
-  const html =  `<li> ${itemValue}</li>`
-  shoppingList.innerHTML += html;
+function displayListItems(item) {
+  let itemID = item[0]
+  let itemValue = item[1]
+
+  let newList = document.createElement('li');
+  newList.textContent = itemValue 
+  shoppingList.appendChild(newList)
+
+  newList.addEventListener('click',function() {
+    let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
+    remove(exactLocationOfItemInDB)
+  } )
 }
 
 function emptyInputField() {
